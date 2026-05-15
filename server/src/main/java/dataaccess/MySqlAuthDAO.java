abd96437bf32144000b24cf159f8ac7ae5a4a150
Nick Brown <nickbro@byu.edu>
2026-05-15 17:07:22 -0600

    P4 Starting MySqlAuthDAO, made readAuth

---
+++ server/src/main/java/dataaccess/MySqlAuthDAO.java
@@ -0,0 +1,111 @@
+package dataaccess;
+
+import models.Auth;
+import models.User;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.*;
+import java.util.UUID;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+
+public class MySqlAuthDAO extends GenericSqlDAO implements AuthDAO {
+
+    public MySqlAuthDAO() throws DataAccessException {
+        super();
+    }
+
+    @Override
+    public Auth createAuth(String username) {
+        var statement = "INSERT INTO auths (username, hashedPassword, email) VALUES (?, ?, ?)";
+
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+        try {
+            executeUpdate(statement, username, hashedPassword, email);
+        }
+        catch (Exception e){
+            throw new DataAccessSQLException("Error: createUser broke", 500);
+        }
+    }
+
+    @Override
+    public void createUser(String username, String password, String email) throws DataAccessSQLException {
+        var statement = "INSERT INTO users (username, hashedPassword, email) VALUES (?, ?, ?)";
+
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+        try {
+            executeUpdate(statement, username, hashedPassword, email);
+        }
+        catch (Exception e){
+            throw new DataAccessSQLException("Error: createUser broke", 500);
+        }
+    }
+
+    @Override
+    public User getUser(String username) throws DataAccessSQLException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, hashedPassword, email FROM users WHERE username=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, username);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readUser(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessSQLException("Error: getUser SQL failed", 500);
+        }
+        return null;
+    }
+
+
+    @Override
+    public User getUser(User user) throws DataAccessSQLException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, hashedPassword, email FROM users WHERE username=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, user.username());
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readUser(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessSQLException("Error: getUser SQL failed", 500);
+        }
+        return null;
+    }
+
+    @Override
+    public void clear() throws DataAccessSQLException{
+        var statement = "TRUNCATE users";
+        try {
+            executeUpdate(statement);
+        }
+        catch (DataAccessException e){
+            throw new DataAccessSQLException("Error: clear failed, awkward",500);
+        }
+    }
+
+    private Auth readAuth(ResultSet rs) throws SQLException{
+        var authToken = rs.getString("authToken");
+        var username = rs.getString("username");
+        return new Auth(UUID.randomUUID().toString(), username);
+    }
+
+    protected final String[] getCreateStatements(){
+        return new String[]{"""
+            CREATE TABLE IF NOT EXISTS games (
+              `authToken` varchar(256) NOT NULL,
+              `username` varchar(256) NOT NULL,
+              PRIMARY KEY (`username`),
+            )
+            """
+        };
+    };
+
+}
--- server/src/main/java/dataaccess/MySqlGameDAO.java
+++ server/src/main/java/dataaccess/MySqlGameDAO.java
@@ -124,7 +124,6 @@ public class MySqlGameDAO extends GenericSqlDAO implements GameDAO{
         )
         """
         };
-    };
-
+    }
 
 }
